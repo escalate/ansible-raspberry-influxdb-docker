2b2b79ab31e600885226e15b958eeab62b9d5161
Felix Boerner <ich@felix-boerner.de>
2021-03-19 09:09:59 +0100

    feat: remove linter ignores

--- molecule/default/tests/conftest.py
+++ molecule/default/tests/conftest.py
@@ -18,5 +18,6 @@ def pytest_runtest_setup(item):
         ).get_hosts("all")
     else:
         pytest.skip(
-            "Test should run only from inside molecule.", allow_module_level=True
+            "Test should run only from inside molecule.",
+            allow_module_level=True
         )
